a9f86600a5ec4beb6d2bbdd0e2534f05e72feab1
Mara Alexeev <mara.alexeev@gmail.com>
2020-10-30 21:33:26 -0400

    Working through comments for each table
    Need to fix a few things in database graph.

---
+++ recipe_create_csv.R
@@ -0,0 +1,8 @@
+recipe_create_csv <- function(data, path = "./csv_files/") {
+  path_name <- paste0(path, deparse(substitute(data)), ".csv")
+
+  write_csv(data, path_name)
+
+  message <-  print("Your dataframe should be converted to a csv and now be saved in the csv_files folder.")
+
+  }
