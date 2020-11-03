aef1648404e39a711af0a8439d8a1791b39ac605
Mara Alexeev <mara.alexeev@gmail.com>
2020-11-03 15:25:36 -0500

    Final proofreading of the write up

--- global.R
+++ global.R
@@ -6,5 +6,10 @@ library(dbplyr)
 library(shinyjs)
 library(digest)
 library(DT)
+library(tidyverse)
 
 con_recipe_db <- dbConnect(RSQLite::SQLite(), "recipesDB.db")
+
+recipe_meta <-read_excel("files/recipe_workbook.xlsx",
+                         sheet = "recipe_meta")
+
